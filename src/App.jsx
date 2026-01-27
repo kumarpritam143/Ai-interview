ed280bded802966918b96f2c945e548ad8ecdf4b
PRITAM KUMAR DAS <kumardaspritam972@gmail.com>
2026-01-28 00:07:51 +0530

    Fix case-sensitive import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-
-import Home from './pages/home'
+import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import NotFound from './pages/NotFound'
